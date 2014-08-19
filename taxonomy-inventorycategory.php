1db9786ca95adc9c7a9a5b9371d2ce55f1b6c414
Tori Holcomb <toriholcomb@spiderVM1.(none)>
2014-08-19 13:49:39 -0400

    lightbox

--- taxonomy-inventorycategory.php
+++ taxonomy-inventorycategory.php
@@ -9,9 +9,17 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();?>
 	<article class="row">
 		<div class="col-md-4"><?php 
 			$gallery = get_post_meta($post->ID, 'daltons_inventory_gallery', true);
+			$i = 0; 
 			foreach ( $gallery as $image ) :
-                $img_src = wp_get_attachment_image_src( $image['gal'], 'full' ); ?>
-                <a href="<?php $img_src[0]; ?>" rel="prettyPhoto[gal-<?php echo $post->ID; ?>]"><img src="<?php echo $img_src[0]; ?>" /></a><?php
+				if ($i==0) : 
+					$img_srct = wp_get_attachment_image_src( $image['gal'], 'thumbnail');
+					$img_src = wp_get_attachment_image_src( $image['gal'], 'full' );?>
+					<a class="img-responsive center-block" href="<?php echo $img_src[0]; ?>" rel="prettyPhoto[gal-<?php echo $post->ID; ?>]"><img class="img-responsive center-block" src="<?php echo $img_srct[0]; ?>" /></a><?php
+				else :
+					$img_src = wp_get_attachment_image_src( $image['gal'], 'full' ); ?>
+                	<a href="<?php echo $img_src[0]; ?>" rel="prettyPhoto[gal-<?php echo $post->ID; ?>]"><img style="display:none;" src="<?php echo $img_src[0]; ?>" /></a><?php
+				endif;
+				$i++;
             endforeach;?>
 			<h2><?php the_title(); ?></h2>
 		</div>
