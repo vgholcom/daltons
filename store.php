fcbba8523e29651296a5c36566eca7d9419f77cf
Tori Holcomb <vgholcom@syr.edu>
2014-10-01 20:29:10 -0400

    category

--- store.php
+++ store.php
@@ -12,29 +12,20 @@ get_header(); ?>
 		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php 
 	endif;
 
-	$categories = get_terms( 'inventorycategory'); ?>
+	$categories = get_terms( 'inventorycategory');
+	$count = count($categories); $I = 0; ?>
 	<div class="row"><?php
-		foreach ($categories as $category) : 
-			if ($category->parent == 0) {
-				$parentID = $category->term_id;?>
-				<h2><?php echo $category->name; ?></h2><?php
-				$child_cat = get_term_children( $parentID,'designercategory' ); 
-				if (!empty($child_cat)) :
-					$count = count($child_cat); $I = 0; 
-					foreach ($child_cat as $child) { $I++;
-						$t_id = $child->term_id;
-				    	$cat_meta = get_option( "category_$t_id");
-				    	$img_src = wp_get_attachment_image_src( $cat_meta['img'], 'full' );
-						$link = get_term_link( $child );?>
-						<a class="col-md-3 inventory" href="<?php echo $link; ?>">
-							<img id="inventory_image" class="img-responsive" src="<?php if($cat_meta['img'] != '') echo $img_src[0]; ?>"/>
-							<h3><?php echo $child->name; ?></h3>
-						</a><?php
-						if ($I % 3 == 0 && $I != $count) {?>
-							</div><?php 
-						}
-					}
-				endif;
+		foreach ($categories as $category) : $I++;
+			$t_id = $category->term_id;
+	    	$cat_meta = get_option( "category_$t_id");
+	    	$img_src = wp_get_attachment_image_src( $cat_meta['img'], 'full' );
+			$link = get_term_link( $category );?>
+			<a class="col-md-3 inventory" href="<?php echo $link; ?>">
+				<img id="inventory_image" class="img-responsive" src="<?php if($cat_meta['img'] != '') echo $img_src[0]; ?>"/>
+				<h3><?php echo $category->name; ?></h3>
+			</a><?php
+			if ($I % 3 == 0 && $I != $count) {?>
+				</div><?php 
 			}
 		endforeach; 
 		if ($I % 3 != 0 ) {?>
